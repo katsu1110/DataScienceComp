5d42483e31e3961616244fcfbae1a534ae758756
Katsuhisa Kawaguchi <kakawagu@yahoo-corp.jp>
2020-03-15 16:55:03 +0900

    nn multiclass

--- code/multiclass_helper.py
+++ code/multiclass_helper.py
@@ -1,20 +1,20 @@
 import numpy as np
 import pandas as pd
 
-def onehot_target(y):
+def onehot_target(y, n_class):
     """
     make one-hot multiclass target 
     """
-    y_cls = np.zeros((len(y), 4))
+    y_cls = np.zeros((len(y), n_class))
     for i, v in enumerate(y):
         y_cls[i, v] = 1
     return y_cls
 
-def ordinal_target(y):
+def ordinal_target(y, n_class):
     """
     make ordinal target
     """
-    y_cls = np.zeros((len(y), 4))
+    y_cls = np.zeros((len(y), n_class))
     for i, v in enumerate(y):
         y_cls[i, :v+1] = 1
     return y_cls
--- models/run_models.py
+++ models/run_models.py
@@ -25,6 +25,7 @@ from nn_param_models import nn_model
 mypath = os.getcwd()
 sys.path.append(mypath + '/code/')
 from train_helper import get_oof_ypred
+from multiclass_helper import onehot_target, ordinal_target
 from cv_methods import GroupKFold, StratifiedGroupKFold
 
 class RunModel(object):
@@ -92,8 +93,13 @@ class RunModel(object):
             train_set = lgb.Dataset(x_train, y_train, categorical_feature=self.categoricals)
             val_set = lgb.Dataset(x_val, y_val, categorical_feature=self.categoricals)
         else:
-            train_set = {'X': x_train, 'y': y_train}
-            val_set = {'X': x_val, 'y': y_val}
+            if (self.model == "nn") & (self.task == "multiclass"):
+                n_class = len(np.unique(self.train_df[self.target].values))
+                train_set = {'X': x_train, 'y': onehot_target(y_train, n_class)}
+                val_set = {'X': x_val, 'y': onehot_target(y_val, n_class)}
+            else:
+                train_set = {'X': x_train, 'y': y_train}
+                val_set = {'X': x_val, 'y': y_val}
         return train_set, val_set
 
     def calc_metric(self, y_true, y_pred): # this may need to be changed based on the metric of interest
