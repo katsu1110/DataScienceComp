26d44f4989e4aa09a86dfcef7f2d907180f4dd89
Katsuhisa Kawaguchi <kakawagu@yahoo-corp.jp>
2020-05-20 09:59:38 +0900

    fix adversarial validation

--- code/adversarial_validaton.py
+++ code/adversarial_validaton.py
@@ -16,27 +16,28 @@ sns.set_context("talk")
 style.use('ggplot')
 
 # adversarial validation
+# adversarial validation
 class AdversarialValidation(object):
     """
     perform adversarial validation using LGB model
 
     :INPUTS:
-
+    
     :train: train pandas dataframe
     :test: test pandas dataframe
     :features: list of feature names
     :n_splits: K in KFold (default is 4)
-
+    
     :EXAMPLE:
-
+    
     # run adversarial validation
     adv = AdversarialValidation(train, test, features, n_splits=4)
-
     # plot features different between train and test
     sorted_df = adv.plot_feature_importance()
     sorted_df.head()
-
+    
     """
+    
     def __init__(self, train : pd.DataFrame, test : pd.DataFrame, features : List, n_splits : int=4, seed : int=116):
         self.train = train
         self.test = test
@@ -57,20 +58,21 @@ class AdversarialValidation(object):
         """
         # list is here: https://lightgbm.readthedocs.io/en/latest/Parameters.html
         params = {
-                    'n_estimators': 2000,
+                    'n_estimators': 800,
                     'objective': 'binary',
                     'boosting_type': 'gbdt',
                     'num_leaves': 32,
-                    'learning_rate': 0.1,
+                    'learning_rate': 0.12,
                     'feature_fraction': 1,
-                    'lambda_l1': 1,
-                    'lambda_l2': 1,
+                    'lambda_l1': 0.8,
+                    'lambda_l2': 0.8,
                     'seed': self.seed,
                     'early_stopping_rounds': 80,
-                    'num_boost_round': 2000
+                    'num_boost_round': 800
                     }    
         params["metric"] = "auc" # other candidates: binary_logloss
         # params["is_unbalance"] = True # assume unbalanced data
+
         return params
             
     def run(self):
@@ -79,21 +81,21 @@ class AdversarialValidation(object):
         """
 
         # train = 1, test = 0
+        target = "is_train"
         df = pd.concat([self.train[self.features], self.test[self.features]],
                         ignore_index=True)
-        df["is_train"] = 0
-        df.loc[:self.train.shape[0], "is_train"] = 1
-        target = "is_train"
+        df[target] = 0
+        df.loc[:self.train.shape[0], target] = 1
 
         # fit LGB
         params = self.get_params()
         kf = StratifiedKFold(n_splits=self.n_splits, random_state=self.seed, shuffle=True)
         fi = np.zeros((self.n_splits, len(self.features)))
-        oof_pred = np.zeros(self.train.shape[0])
-        for fold, (train_index, test_index) in enumerate(kf.split(self.train, self.train[target])):
+        oof_pred = np.zeros(df.shape[0])
+        for fold, (train_index, test_index) in enumerate(kf.split(df, df[target])):
             # train test split
-            x_train, x_val = self.train[train_index, self.features], self.train[test_index, self.features]
-            y_train, y_val = self.train[train_index, target], self.train[test_index, target]
+            x_train, x_val = df.loc[train_index, self.features], df.loc[test_index, self.features]
+            y_train, y_val = df.loc[train_index, target], df.loc[test_index, target]
 
             # model fitting
             train_set = lgb.Dataset(x_train, y_train)
@@ -105,8 +107,8 @@ class AdversarialValidation(object):
             oof_pred[test_index] = model.predict(x_val)
             print('Partial score of fold {} is: {}'.format(fold, self.calc_metric(y_val, oof_pred[test_index])))
 
-        score = self.calc_metric(self.train[target], oof_pred)
-        print('Our oof cv score is: ', score)
+        score = self.calc_metric(df[target], oof_pred)
+        print('Our oof cv score (AUC) is: ', score)
 
         # feature importance data frame
         fi_df = pd.DataFrame()
@@ -125,6 +127,7 @@ class AdversarialValidation(object):
         """
         function for plotting feature importance
         """
+        
         # plot feature importance
         _, ax = plt.subplots(1, 1, figsize=(10, 20))
         sorted_df = self.fi_df.sort_values(by = "importance_mean", ascending=False).reset_index().iloc[self.n_splits * (rank_range[0]-1) : self.n_splits * rank_range[1]]
