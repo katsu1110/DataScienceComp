e49ac0f3cc8dd308a4ecfcdfe7712b6dbb248870
Katsuhisa Kawaguchi <kakawagu@yahoo-corp.jp>
2020-06-08 10:37:31 +0900

    nn libraries and params modified

--- models/nn_param_models.py
+++ models/nn_param_models.py
@@ -1,17 +1,17 @@
 import numpy as np
 import pandas as pd
 import os, sys
+import math
 
 # keras
 import tensorflow as tf
-from tensorflow.keras.callbacks import Callback, EarlyStopping, ReduceLROnPlateau, LambdaCallback
-from tensorflow.keras.optimizers import Adam, SGD
-from tensorflow.keras.models import Model, Sequential
-from tensorflow.keras import losses, models, optimizers
-from tensorflow.keras.utils import get_custom_objects
-from tensorflow.keras.layers import Input, Layer, Dense, Activation, Embedding, Concatenate, Reshape, Dropout, Add, BatchNormalization, LayerNormalization, GaussianNoise
 from tensorflow.keras import backend as K
-import math
+from tensorflow.keras import callbacks
+from tensorflow.keras import optimizers
+from tensorflow.keras import models
+from tensorflow.keras import losses
+from tensorflow.keras import utils
+from tensorflow.keras import layers
 
 # enable mish
 class Mish(tf.keras.layers.Layer):
@@ -31,9 +31,9 @@ class Mish(tf.keras.layers.Layer):
         return input_shape
 
 def mish(x):
-	return tf.keras.layers.Lambda(lambda x: x*K.tanh(K.softplus(x)))(x)
+	return layers.Lambda(lambda x: x*K.tanh(K.softplus(x)))(x)
 
-get_custom_objects().update({'mish': Activation(mish)})
+utils.get_custom_objects().update({'mish': layers.Activation(mish)})
 
 def nn_model(cls, train_set, val_set):
     """
@@ -44,13 +44,14 @@ def nn_model(cls, train_set, val_set):
     params = {
         'input_dropout': 0.0,
         'hidden_layers': 2,
-        'hidden_units': 128,
+        'hidden_units': 64,
         'embedding_out_dim': 4,
         'hidden_activation': 'mish', 
         'hidden_dropout': 0.04,
+        'gauss_noise': 0.01,
         'norm_type': 'batch', # layer
-        'optimizer': {'type': 'adam', 'lr': 1e-4},
-        'batch_size': 128,
+        'optimizer': {'type': 'adam', 'lr': 1e-3},
+        'batch_size': 64,
         'epochs': 40
     }
 
@@ -63,56 +64,58 @@ def nn_model(cls, train_set, val_set):
         embeddings = []
         embedding_out_dim = params['embedding_out_dim']
         for i in cls.categoricals:
-            input_ = Input(shape=(1,))
-            embedding = Embedding(int(np.absolute(cls.train_df[i]).max() + 1), embedding_out_dim, input_length=1)(input_)
-            embedding = Reshape(target_shape=(embedding_out_dim,))(embedding)
+            input_ = layers.Input(shape=(1,))
+            embedding = layers.Embedding(int(np.absolute(cls.train_df[i]).max() + 1), embedding_out_dim, input_length=1)(input_)
+            embedding = layers.Reshape(target_shape=(embedding_out_dim,))(embedding)
             inputs.append(input_)
             embeddings.append(embedding)
-        input_numeric = Input(shape=(len(cls.features) - len(cls.categoricals),))
-        embedding_numeric = Dense(n_neuron, activation=params['hidden_activation'])(input_numeric)
+        input_numeric = layers.Input(shape=(len(cls.features) - len(cls.categoricals),))
+        embedding_numeric = layers.Dense(n_neuron, activation=params['hidden_activation'])(input_numeric)
         inputs.append(input_numeric)
         embeddings.append(embedding_numeric)
-        x = Concatenate()(embeddings)
+        x = layers.Concatenate()(embeddings)
 
     else: # no categorical features
-        inputs = Input(shape=(len(cls.features), ))
-        x = Dense(n_neuron, activation=params['hidden_activation'])(inputs)
-        x = Dropout(params['hidden_dropout'])(x)
+        inputs = layers.Input(shape=(len(cls.features), ))
+        x = layers.Dense(n_neuron, activation=params['hidden_activation'])(inputs)
+        x = layers.Dropout(params['hidden_dropout'])(x)
+        x = layers.GaussianNoise(params['gauss_noise'])(x)
         if params['norm_type'] == 'batch':
-            x = BatchNormalization()(x)
+            x = layers.BatchNormalization()(x)
         elif params['norm_type'] == 'layer':
-            x = LayerNormalization()(x)
+            x = layers.LayerNormalization()(x)
         
     # more layers
     for i in np.arange(params['hidden_layers'] - 1):
-        x = Dense(n_neuron // (2 * (i+1)), activation=params['hidden_activation'])(x)
-        x = Dropout(params['hidden_dropout'])(x)
+        x = layers.Dense(n_neuron // (2 * (i+1)), activation=params['hidden_activation'])(x)
+        x = layers.Dropout(params['hidden_dropout'])(x)
+        x = layers.GaussianNoise(params['gauss_noise'])(x)
         if params['norm_type'] == 'batch':
-            x = BatchNormalization()(x)
+            x = layers.BatchNormalization()(x)
         elif params['norm_type'] == 'layer':
-            x = LayerNormalization()(x)
+            x = layers.LayerNormalization()(x)
     
     # output
     if cls.task == "regression":
-        out = Dense(1, activation="linear", name = "out")(x)
+        out = layers.Dense(1, activation="linear", name = "out")(x)
         loss = "mse"
     elif cls.task == "binary":
-        out = Dense(1, activation='sigmoid', name = 'out')(x)
+        out = layers.Dense(1, activation='sigmoid', name = 'out')(x)
         loss = "binary_crossentropy"
     elif cls.task == "multiclass":
-        out = Dense(len(np.unique(cls.train_df[cls.target].values)), activation='softmax', name = 'out')(x)
+        out = layers.Dense(len(np.unique(cls.train_df[cls.target].values)), activation='softmax', name = 'out')(x)
         loss = "categorical_crossentropy"
-    model = Model(inputs=inputs, outputs=out)
+    model = models.Model(inputs=inputs, outputs=out)
 
     # compile
     if params['optimizer']['type'] == 'adam':
-        model.compile(loss=loss, optimizer=Adam(lr=params['optimizer']['lr'], beta_1=0.9, beta_2=0.999, decay=1e-04))
+        model.compile(loss=loss, optimizer=optimizers.Adam(lr=params['optimizer']['lr'], beta_1=0.9, beta_2=0.999, decay=params['optimizer']['lr']/100))
     elif params['optimizer']['type'] == 'sgd':
-        model.compile(loss=loss, optimizer=SGD(lr=params['optimizer']['lr'], decay=1e-6, momentum=0.9))
+        model.compile(loss=loss, optimizer=optimizers.SGD(lr=params['optimizer']['lr'], decay=1e-6, momentum=0.9))
 
     # callbacks
-    er = EarlyStopping(patience=8, min_delta=1e-4, restore_best_weights=True, monitor='val_loss')
-    ReduceLR = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=8, verbose=1, epsilon=1e-4, mode='min')
+    er = callbacks.EarlyStopping(patience=8, min_delta=params['optimizer']['lr'], restore_best_weights=True, monitor='val_loss')
+    ReduceLR = callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=8, verbose=1, epsilon=params['optimizer']['lr'], mode='min')
     history = model.fit(train_set['X'], train_set['y'], callbacks=[er, ReduceLR],
                         epochs=params['epochs'], batch_size=params['batch_size'],
                         validation_data=[val_set['X'], val_set['y']])        
